4f23d2e9e3319a592aa61283094cbc3f25330f69
Fernando Costa <fadc80@gmail.com>
2019-05-21 07:46:58 -0300

    Adding help comments

--- template/service.groovy
+++ template/service.groovy
@@ -1,5 +1,9 @@
+// Code generated from template.
 def handle(global) {
   global.with {
+    // Replace below code with your code. 
+    // Inside this block all Imposter 
+    // methods are availible.
     logger.info("Service handler: <!--SERVICE-->")
     respond {
       withStatusCode(200) and()
